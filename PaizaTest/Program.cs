3d5880e5dc65dd14e11ebc1c1eebc215463cc82b
yas <the.world.is.large@gmail.com>
2016-12-11 02:29:30 +0900

    clean code

--- PaizaTest/Program.cs
+++ PaizaTest/Program.cs
@@ -7,15 +7,7 @@ using System.Threading.Tasks;
 namespace PaizaTest {
     public class Program {
         public static void Main(string[] args) {
-            var arg = Console.ReadLine().Split(' ');
-            var val = new int[] { int.Parse(arg[0]), int.Parse(arg[1]) };
 
-            var r1 = Math.Pow(val[0], 3);
-            var r2 = Math.Pow(val[1], 3);
-
-            var result = r1 - r2;
-
-            Console.WriteLine(result);
         }
 
         static System.IO.StreamWriter sw = new System.IO.StreamWriter("output.txt",false);
