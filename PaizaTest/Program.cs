61e794df32e4f2d48a8f9940c9bac9724cb8e3e6
yas <the.world.is.large@gmail.com>
2016-12-11 02:09:10 +0900

    create test

---
+++ PaizaTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaizaTest {
+    public class Program {
+        public static void Main(string[] args) {
+            var arg = Console.ReadLine().Split(' ');
+            var val = new int[] { int.Parse(arg[0]), int.Parse(arg[1]) };
+
+            var r1 = Math.Pow(val[0], 3);
+            var r2 = Math.Pow(val[1], 3);
+
+            var result = r1 - r2;
+
+            Console.WriteLine(result);
+        }
+
+        static System.IO.StreamWriter sw = new System.IO.StreamWriter("output.txt",false);
+
+        static Program() {
+            Console.SetIn(new System.IO.StreamReader("input.txt"));
+
+            //var sw = new System.IO.StreamWriter("output.txt",false);
+            //sw.AutoFlush = true;
+
+            Console.SetOut(sw);
+        }
+
+        public static System.IO.StreamReader GetExpectedAnswerStreamReader() {
+            return new System.IO.StreamReader("answer.txt");
+        }
+
+        public static System.IO.StreamReader GetFactAnswerStreamReader() {
+            return new System.IO.StreamReader("output.txt");
+        }
+
+        public static void CloseStreamReader() {
+            sw.Close();
+        }
+
+    }
+
+}
---
+++ PaizaTestTests/ProgramTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaizaTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaizaTest.Tests {
+    [TestClass()]
+    public class ProgramTests {
+        [TestMethod()]
+        public void TestTest() {
+            PaizaTest.Program.Main(null);
+            PaizaTest.Program.Main(null);
+            PaizaTest.Program.Main(null);
+
+            PaizaTest.Program.CloseStreamReader();
+
+            var stream_expected = PaizaTest.Program.GetExpectedAnswerStreamReader();
+            var stream_fact = PaizaTest.Program.GetFactAnswerStreamReader();
+
+            string expected;
+            string fact;
+            int no = 1;
+            while((expected = stream_expected.ReadLine()) != null) {
+                fact = stream_fact.ReadLine();
+                Assert.AreEqual(expected, fact, String.Format("No.{0}", no, expected, fact));
+                no++;
+            }
+
+        }
+    }
+}
